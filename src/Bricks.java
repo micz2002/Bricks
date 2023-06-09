988fa6b4517899855c624515e89b657d020f4d79
Jakub Fałek <jakub2002pl@wp.pl>
2023-06-09 15:56:19 +0200

    Added map to manage

--- src/Bricks.java
+++ src/Bricks.java
@@ -1,10 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Bricks {
     public static void main(String[] args) {
+        List<String> freeBlocks = new ArrayList<>();
+        Map<Integer, List<String>> instructions = new HashMap<>();
+
         File file = new File("plik.txt");
+
         if(file.exists()) {
             System.out.println("Wczytywanie danych z pliku");
             Scanner dataFromFile = null;
@@ -20,11 +24,36 @@ public class Bricks {
                 String[] parts = line.split(":");
                 int number = Integer.parseInt(parts[0]);
                 String code = parts[1];
+                //////Zliczanie i zarzadzanie klockami
+                if(number == 0) {
+                    freeBlocks.add(code);
+                } else {
+                    if(instructions.containsKey(number)) {
+                        List<String> listOfCode  = instructions.get(number);
+                        listOfCode.add(code);
+                    } else {
+                        List<String> listOfCode = new ArrayList<>();
+                        listOfCode.add(code);
+                        instructions.put(number, listOfCode);
+                    }
+                }
                 System.out.println("tu numer: " + number + ", a tu kod: " + code);
             }
-        } else{
-            //Wprowadz dane recznie, reszta programu taka sama
-        }
+            System.out.println("????????????????????????????????????????????????????");
+            System.out.println("WOLNE KLOCKI: ");
+            for (String freeBlock : freeBlocks) {
+                System.out.println(freeBlock);
+            }
+
+            for(Map.Entry<Integer, List<String>> entry : instructions.entrySet()) {
+                Integer keyInstructions = entry.getKey();
+                List<String> valueInstructions = entry.getValue();
+                System.out.println("Klucz: " + keyInstructions);
+                System.out.println("Wartosci: " + valueInstructions);
+            }
+          }// else{
+//            //Wprowadz dane recznie, reszta programu taka sama
+//        }
 
     }
 }
